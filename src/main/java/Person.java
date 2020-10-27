17b9b5aaa740a61d2a202d96ac316debbeb95a31
Reevak05 <kaveergera@gmail.com>
2020-10-27 15:39:39 +0000

    create Person class and add related methods

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,38 @@
 public class Person {
+    private String firstName;
+    private String lastName;
+
+    public Person(final String a, final String b){
+        firstName = a;
+        lastName = b;
+    }
+
     
+    public String doSomething() {
+        return this.firstName + " " + this.lastName + " is doing something";
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setFirstName(final String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(final String lastName) {
+        this.firstName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return this.firstName + " " + this.lastName;
+    }
+
 }
 
 
--- src/main/java/PersonClient.java
+++ src/main/java/PersonClient.java
@@ -1,5 +1,7 @@
 public class PersonClient {
     public static void main(String[] args) {
-        
+        Person p = new Person("Kaveer", "Gera");
+        System.out.println(p.doSomething());
+        System.out.println(p);
     }
 }
